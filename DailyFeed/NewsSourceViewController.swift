efe8cf4591089654f6f33c10814766e387beebbd
Sumit Paul <paulsumit1993@gmail.com>
2016-12-31 21:58:12 +0530

    fix typo

--- DailyFeed/NewsSourceViewController.swift
+++ DailyFeed/NewsSourceViewController.swift
@@ -29,7 +29,7 @@ class NewsSourceViewController: UIViewController, UITableViewDelegate, UITableVi
             controller.dimsBackgroundDuringPresentation = false
             controller.hidesNavigationBarDuringPresentation = false
             controller.searchBar.placeholder = "Search NEWS..."
-            controller.searchBar..searchBarStyle = .Minimal
+            controller.searchBar.searchBarStyle = .Minimal
             controller.searchBar.sizeToFit()
             
             self.sourceTableView.tableHeaderView = controller.searchBar
