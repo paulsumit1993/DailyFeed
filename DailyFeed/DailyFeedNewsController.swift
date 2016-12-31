c69f3e7af183088ff1ff98b8815b20da9738453b
Sumit Paul <paulsumit1993@gmail.com>
2016-12-31 22:55:57 +0530

    search bar pot. fix

--- DailyFeed/DailyFeedNewsController.swift
+++ DailyFeed/DailyFeedNewsController.swift
@@ -58,6 +58,12 @@ class DailyFeedNewsController: UICollectionViewController {
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
         navigationController?.hidesBarsOnTap = false
+        self.controller.searchBar.isHidden = false
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+         super.viewWillDisappear(animated)
+         self.controller.searchBar.isHidden = true
     }
     
     
