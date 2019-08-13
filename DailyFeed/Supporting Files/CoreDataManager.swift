cfe7a09fd966290b33ab1e9d2b91ecd5c6b7c34c
Sumit Paul <paulsumit1993@gmail.com>
2019-08-13 22:58:48 +0530

    init core data

---
+++ DailyFeed/Supporting Files/CoreDataManager.swift
@@ -0,0 +1,77 @@
+//
+//  CoreDataManager.swift
+//  CoreDataStack
+//
+//  Created by Bart Jacobs on 17/07/16.
+//  Copyright © 2016 Cocoacasts. All rights reserved.
+//
+import CoreData
+import Foundation
+
+public class CoreDataManager {
+    
+    private let modelName: String
+    
+    init(modelName: String) {
+        self.modelName = modelName
+    }
+    
+    // MARK: - Core Data Stack
+    public private(set) lazy var managedObjectContext: NSManagedObjectContext = {
+        // Initialize Managed Object Context
+        let managedObjectContext = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+        
+        // Configure Managed Object Context
+        managedObjectContext.persistentStoreCoordinator = self.persistentStoreCoordinator
+        
+        return managedObjectContext
+    }()
+    
+    private lazy var managedObjectModel: NSManagedObjectModel? = {
+        // Fetch Model URL
+        guard let modelURL = Bundle.main.url(forResource: self.modelName, withExtension: "momd") else {
+            return nil
+        }
+        
+        // Initialize Managed Object Model
+        let managedObjectModel = NSManagedObjectModel(contentsOf: modelURL)
+        
+        return managedObjectModel
+    }()
+    
+    private lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator? = {
+        guard let managedObjectModel = self.managedObjectModel else {
+            return nil
+        }
+        
+        // Helper
+        let persistentStoreURL = self.persistentStoreURL
+        
+        // Initialize Persistent Store Coordinator
+        let persistentStoreCoordinator = NSPersistentStoreCoordinator(managedObjectModel: managedObjectModel)
+        
+        do {
+            let options = [ NSMigratePersistentStoresAutomaticallyOption : true, NSInferMappingModelAutomaticallyOption : true ]
+            try persistentStoreCoordinator.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: persistentStoreURL as URL, options: options)
+            
+        } catch {
+            let addPersistentStoreError = error as NSError
+            
+            print("Unable to Add Persistent Store")
+            print("\(addPersistentStoreError.localizedDescription)")
+        }
+        
+        return persistentStoreCoordinator
+    }()
+    
+    // MARK: - Computed Properties
+    private var persistentStoreURL: NSURL {
+        // Helpers
+        let storeName = "\(modelName).sqlite"
+        let fileManager = FileManager.default
+        
+        let documentsDirectoryURL = fileManager.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        return documentsDirectoryURL.appendingPathComponent(storeName) as NSURL
+    }
+    
+}
